4a7c4b6e622b6d4e1ad412f0bc94b81d97699f3a
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-08 15:55:47 +0700

    update solution

---
+++ 01-Variable/02/main.js
@@ -0,0 +1,20 @@
+// - ตั้งชื่อตัวแปรที่ใช้เก็บจำนวนเงินในกระเป๋าตังของคุณ
+// - ตั้งชื่อตัวแปรที่ใช้เก็บชื่อของ พ่อและแม่ของคุณ
+// - ตั้งชื่อตัวแปรที่ใช้เก็บที่อยู่ของบ้านคุณ
+// - ตั้งชื่อตัวแปรที่ใช้เก็บอายุของจักรวาล
+
+// Naming => Convention camelCase
+
+let myWallet = -20;
+const myFatherName = 'John Doe';
+let myMotherName = 'Martha Doe';
+var myAddress = 'Terminal 9-3/4';
+let ageOfUniverse = 13_700_000_000;
+
+myFriend = 'Jack Ma'; // no keyword = var
+myHobby; // myHobby = undefined
+
+// Bad Practices : ชื่อตัวแปรไม่สื่อความหมาย / สื่อถึงข้อมูลที่เก็บไว้
+// let a = -20;
+// let b = 'John Doe';
+// let c = 'Martha Doe';
---
+++ 01-Variable/04/main.js
@@ -0,0 +1,20 @@
+// - ประกาศตัวแปรเป็น const ชื่อ firstName มีค่าเป็นชื่อจริงตัวเอง
+// - ประกาศตัวแปรเป็น let ชื่อ lastName มีค่าเป็นนามสกุลตัวเอง
+// - ประกาศตัวแปรเป็น var ชื่อ nickname มีค่าเป็นชื่อเล่นตัวเอง
+// - ประกาศตัวแปรทั้ง 3 ซ้ำอีกรอบโดยใช้ชื่อตัวแปรเดิม และให้ใส่ข้อมูลของเพื่อนลงในตัวแปร
+// - ประกาศตัวแปรชื่อ birthDate โดยไม่ต้องใส่ const let หรือ var นำหน้าชื่อตัวแปร และกำหนดค่าเริ่มต้นเป็นวันเกิดของตัวเอง
+
+// ReDeclaration การประกาศแบบใช้ชื่อซ้ำ
+// ชื่อตัวแปร = variable == Identity == Identifier
+
+// const firstName = 'pavit';
+// const firstName = 'john';
+
+// let lastName = 'pimchanangul';
+// let lastName = 'doe';
+
+// Var สามารถประกาศชื่อซ้ำได้
+var nickname = 'We';
+var nickname = 'John';
+
+console.log(nickname);
